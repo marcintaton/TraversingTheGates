afe771c2fcf9bbd8061cb62c899fa19320d4c9c6
nefarinus <tatonmarcinpr@gmail.com>
2019-10-31 19:17:07 +0100

    refactoring and further fixes

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ class Entity
 class enemy;
 
 template<typename... T_args>
-struct IdentifiedFunction {
-    size_t id;
+struct TrackedFunction {
+    size_t tracking_id;
     std::function<void(T_args...)> function;
 };
 
@@ -26,44 +26,58 @@ class Event;
 template<typename... T_args>
 struct EventSubscriptionBind {
     Event<T_args...> subscribed_event;
-    size_t id;
+    size_t tracked_listener_id;
+};
+
+enum class EventType {
+    error_type,
+    on_player_death,
+    on_level_loaded,
+    on_new_game_started,
+    on_game_saved,
+    on_end_of_level_reached
 };
 
 template<typename... T_args>
 class Event
 {
   private:
-    size_t function_index = 0;
-    std::vector<IdentifiedFunction<T_args...>> functions;
+    size_t tracking_function_index = 0;
+    std::vector<TrackedFunction<T_args...>> listeners;
 
   public:
-    int event_id;
+    Event() : event_type(EventType::error_type) {}
+    Event(EventType type) : event_type(type) {}
+
+    const EventType event_type;
     EventSubscriptionBind<T_args...>
-    Subscribe(std::function<void(T_args...)> subscriber)
+    subscribe(std::function<void(T_args...)> subscriber)
     {
-        IdentifiedFunction<T_args...> identified_subscriber =
-            IdentifiedFunction<T_args...>(
-                {.id = ++function_index, .function = subscriber});
-        functions.push_back(identified_subscriber);
+        TrackedFunction<T_args...> identified_subscriber =
+            TrackedFunction<T_args...>(
+                {.tracking_id = ++tracking_function_index,
+                 .function = subscriber});
+        listeners.push_back(identified_subscriber);
 
         EventSubscriptionBind<T_args...> bind = {.subscribed_event = *this,
-                                                 .id = function_index};
+                                                 .tracked_listener_id =
+                                                     tracking_function_index};
 
         return bind;
     }
 
     void Unsubscribe(size_t bind_index)
     {
-        auto erase_iterator =
-            std::find_if(functions.begin(), functions.end(),
-                         [&](const auto& val) { return val.id == bind_index; });
+        auto erase_iterator = std::find_if(
+            listeners.begin(), listeners.end(),
+            [&](const auto& val) { return val.tracking_id == bind_index; });
 
-        functions.erase(erase_iterator);
+        listeners.erase(erase_iterator);
     }
 
     void Invoke(T_args... args)
     {
-        for (auto identifier : functions) {
+        for (auto identifier : listeners) {
             identifier.function(std::forward<T_args>(args)...);
         }
     }
@@ -72,15 +86,17 @@ class Event
 template<typename... T_args>
 bool operator==(const Event<T_args...>& a, const Event<T_args...>& b)
 {
-    return a.event_id == b.event_id;
+    return a.event_type == b.event_type;
 }
 
 class EventManager
 {
   public:
-    Event<int, int> some_event_1;
-    Event<bool> some_event_2;
-    Event<> some_event_3;
+    Event<int, int> some_event_1 =
+        Event<int, int>(EventType::on_end_of_level_reached);
+    Event<bool> some_event_2 = Event<bool>(EventType::on_game_saved);
+    Event<> some_event_3 = Event<>(EventType::on_new_game_started);
+    Event<int, int> some_event_4 = Event<int, int>(EventType::on_player_death);
 };
 
 EventManager event_manager;
@@ -88,22 +104,23 @@ EventManager event_manager;
 class IEventSubscriber
 {
   private:
-    std::vector<std::any> binds;
+    std::vector<std::any> subscription_binds;
 
   public:
     template<typename... T_args>
     void subscribe(Event<T_args...>& to_event,
                    std::function<void(T_args...)> function)
     {
-        EventSubscriptionBind<T_args...> bind = to_event.Subscribe(function);
-        binds.push_back(bind);
+        EventSubscriptionBind<T_args...> bind = to_event.subscribe(function);
+        subscription_binds.push_back(bind);
     }
 
     template<typename... T_args>
     void unsubscribe(Event<T_args...>& from_event)
     {
-        auto bind_it =
-            std::find_if(binds.begin(), binds.end(), [&](const auto& bind) {
+        auto bind_iterator = std::find_if(
+            subscription_binds.begin(), subscription_binds.end(),
+            [&](const auto& bind) {
                 try {
                     return (std::any_cast<EventSubscriptionBind<T_args...>>(
                                 bind))
@@ -114,8 +131,9 @@ class IEventSubscriber
             });
 
         from_event.Unsubscribe(
-            (std::any_cast<EventSubscriptionBind<T_args...>>(*bind_it)).id);
-        binds.erase(bind_it);
+            (std::any_cast<EventSubscriptionBind<T_args...>>(*bind_iterator))
+                .tracked_listener_id);
+        subscription_binds.erase(bind_iterator);
     }
 };
 
@@ -133,6 +151,7 @@ class enemy : public Entity<enemy>, public IEventSubscriber
             std::bind(&enemy::int_function, this, std::placeholders::_1,
                       std::placeholders::_2);
         subscribe<int, int>(event_manager.some_event_1, f1_bind);
+        subscribe<int, int>(event_manager.some_event_4, f1_bind);
 
         std::function<void(bool)> f2_bind =
             std::bind(&enemy::bool_function, this, std::placeholders::_1);
@@ -173,7 +192,7 @@ int main(void)
     event_manager.some_event_2.Invoke(false);
     std::cout << std::endl;
 
-    e1.unsubscribe<int, int>(event_manager.some_event_1);
+    e1.unsubscribe<int, int>(event_manager.some_event_4);
     e2.unsubscribe<bool>(event_manager.some_event_2);
 
     event_manager.some_event_1.Invoke(40, 5);
