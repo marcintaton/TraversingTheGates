4c13baad1df0d8b7490ad390dbf082cfea7b618d
nefarinus <tatonmarcinpr@gmail.com>
2019-10-31 15:55:17 +0100

    still prototyping events

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <any>
 #include <functional>
 #include <iostream>
 #include <string>
@@ -10,73 +12,146 @@ class Entity
 {
 };
 
-class enemy : public Entity<enemy>
-{
-  private:
-    int id;
+class enemy;
 
-  public:
-    enemy(int _id) { this->id = _id; }
-    void announce(int announcment, int shieet)
-    {
-        std::cout << announcment << shieet << " " << id << std::endl;
-    }
+template<typename... T_args>
+struct IdentifiedFunction {
+    size_t id;
+    std::function<void(T_args...)> function;
+};
+
+template<typename... T_args>
+class Event;
+
+template<typename... T_args>
+struct EventSubscriptionBind {
+    Event<T_args...> subscribed_event;
+    size_t id;
 };
 
 template<typename... T_args>
 class Event
 {
   private:
-    std::vector<std::function<void(T_args...)>> functions;
+    size_t function_index = 0;
+    std::vector<IdentifiedFunction<T_args...>> functions;
 
   public:
-    void Subscribe(std::function<void(T_args...)> subscriber)
+    int event_id;
+    EventSubscriptionBind<T_args...>
+    Subscribe(std::function<void(T_args...)> subscriber)
     {
-        functions.push_back(subscriber);
+        IdentifiedFunction<T_args...> identified_subscriber =
+            IdentifiedFunction<T_args...>(
+                {.id = ++function_index, .function = subscriber});
+        functions.push_back(identified_subscriber);
+
+        EventSubscriptionBind<T_args...> bind = {.subscribed_event = *this,
+                                                 .id = function_index};
+
+        return bind;
     }
 
-    void Unsubscribe(std::function<void(T_args...)> to_unsub) {}
+    void Unsubscribe(size_t bind_index)
+    {
+        auto erase_iterator =
+            std::find_if(functions.begin(), functions.end(),
+                         [&](const auto& val) { return val.id == bind_index; });
+
+        functions.erase(erase_iterator);
+    }
 
     void Invoke(T_args... args)
     {
-        for (auto function : functions) {
-            function(std::forward<T_args>(args)...);
+        for (auto identifier : functions) {
+            identifier.function(std::forward<T_args>(args)...);
         }
     }
 };
+
+template<typename... T_args>
+bool operator==(const Event<T_args...>& a, const Event<T_args...>& b)
+{
+    return a.event_id == b.event_id;
+}
 
 class EventManager
 {
   public:
     Event<int, int> some_event_1;
-    Event<> some_event_2;
+    // static Event<> some_event_2;
 };
 
-void xtuff(int a, int b) {}
+EventManager event_manager;
+
+template<typename... T_args>
+class IEventSubscriber
+{
+  private:
+    std::vector<EventSubscriptionBind<T_args...>> binds;
+
+  public:
+    void subscribe(Event<T_args...>& to_event,
+                   std::function<void(T_args...)> function)
+    {
+        EventSubscriptionBind<T_args...> bind = to_event.Subscribe(function);
+        binds.push_back(bind);
+    }
+
+    void unsubscribe(Event<T_args...>& from_event)
+    {
+        auto bind_it =
+            std::find_if(binds.begin(), binds.end(), [&](const auto& bind) {
+                return bind.subscribed_event == from_event;
+            });
+
+        from_event.Unsubscribe((*bind_it).id);
+        binds.erase(bind_it);
+    }
+};
+
+class enemy : public Entity<enemy>, public IEventSubscriber<int, int>
+{
+  private:
+    int id;
+
+  public:
+    enemy(int _id)
+    {
+        this->id = _id;
+        subscribe(event_manager.some_event_1,
+                  std::bind(&enemy::announce, this, std::placeholders::_1,
+                            std::placeholders::_2));
+    }
+
+    void announce(int announcment, int shieet)
+    {
+        std::cout << announcment << shieet << " " << id << std::endl;
+    }
+};
 
 int main(void)
 {
     // std::cout << typeid(enemy).name() << std::endl;
     // std::cout << typeid(Entity<enemy>).name() << std::endl;
 
-    EventManager manager;
+    enemy e1(0);
+    enemy e2(1);
 
-    enemy e1(0), e2(1);
+    // manager.some_event_1.Subscribe(std::bind(
+    //     &enemy::announce, &e2, std::placeholders::_1,
+    //     std::placeholders::_2));
 
-    manager.some_event_1.Subscribe(std::bind(
-        &enemy::announce, &e1, std::placeholders::_1, std::placeholders::_2));
-    manager.some_event_1.Subscribe(std::bind(
-        &enemy::announce, &e2, std::placeholders::_1, std::placeholders::_2));
-    manager.some_event_1.Subscribe(xtuff);
+    // manager.some_event_1.Invoke(42, 5);
 
-    manager.some_event_1.Invoke(42, 5);
+    // manager.some_event_1.Unsubscribe(bind_1);
+
+    event_manager.some_event_1.Invoke(42, 5);
 
     std::cout << std::endl;
+    e1.unsubscribe(event_manager.some_event_1);
 
-    manager.some_event_1.Unsubscribe(std::bind(
-        &enemy::announce, &e2, std::placeholders::_1, std::placeholders::_2));
-
-    manager.some_event_1.Invoke(42, 5);
+    event_manager.some_event_1.Invoke(40, 5);
 
     return 0;
 }
