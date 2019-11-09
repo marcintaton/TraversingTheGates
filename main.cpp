4df5335887e63c394f698e615dc8b3f83983db3d
nefarinus <tatonmarcinpr@gmail.com>
2019-11-09 20:01:18 +0100

    cleand main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include "src/engine/utility/Logging.h"
 
-// libs
-#include <bitset>
-#include <functional>
-#include <iostream>
-#include <memory>
-#include <typeinfo>
-#include <vector>
-
-// files
-
 int main(void)
 {
 
