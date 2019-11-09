e29be50b18a7336e815cd4ecb211f3e77fee7c1a
nefarinus <tatonmarcinpr@gmail.com>
2019-11-09 13:56:32 +0100

    added declarations of engine test functions

--- tests/engine/Test.h
+++ tests/engine/Test.h
@@ -7,5 +7,32 @@ namespace Tests
 namespace Engine
 {
 void mockup_test();
+
+void test_create_entity();
+void test_get_entities_of_type();
+void test_remove_entity();
+void test_get_entity();
+
+void test_add_component();
+void test_get_components_of_type();
+void test_remove_component();
+void test_get_component();
+void test_get_component_cluster();
+
+void test_create_system();
+void test_create_active_system();
+void test_destroy_system();
+void test_enable_system();
+void test_disable_system();
+
+void test_create_independent_syste();
+void test_destroy_independent_syste();
+void test_enable_independent_syste();
+void test_disable_independent_syste();
+void test_get_independent_system();
+
+void test_send_event();
+void test_add_listener();
+void test_remove_listener();
 }; // namespace Engine
 }; // namespace Tests
