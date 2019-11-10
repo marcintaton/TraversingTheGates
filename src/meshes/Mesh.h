bb90fb7510103e05e819193b2899e5bc274d0762
nefarinus <tatonmarcinpr@gmail.com>
2019-11-10 10:58:41 +0100

    reworked meshes to use vectorss

--- src/meshes/Mesh.h
+++ src/meshes/Mesh.h
@@ -1,8 +1,19 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
+#include <vector>
 
 struct Mesh {
-    GLfloat* vertices;
-    GLuint* indices;
+    std::vector<GLfloat> vertices;
+    std::vector<Gluint> indices;
+
+    inline GLsizeiptr get_vertices_size()
+    {
+        return vertices.size() * sizeof(vertices);
+    }
+
+    inline GLsizeiptr get_indices_size()
+    {
+        return indices.size() * sizeof(indices);
+    }
 };
--- src/meshes/Quad.h
+++ src/meshes/Quad.h
@@ -5,26 +5,28 @@
 
 struct Quad : Mesh {
 
-    GLfloat vertices[20] = {
-        // top right
-        0.5f, 0.5f, 0.0f, // position
-        1.0f, 1.0f,       // uv
-        // bottom right
-        0.5f, -0.5f, 0.0f, // position
-        1.0f, 0.0f,        // uv
-        // bottom left
-        -0.5f, -0.5f, 0.0f, // position
-        0.0f, 0.0f,         // uv
-        // top left
-        -0.5f, 0.5f, 0.0f, // position
-        0.0f,
-        1.0f, // uv
-    };
+    Quad()
+    {
 
-    GLuint indices[6] = {
-        0, 1, 3, // first tris
-        1, 2, 3  // second tris
-    };
+        vertices = {
+            // top right
+            0.5f, 0.5f, 0.0f, // position
+            1.0f, 1.0f,       // uv
+            // bottom right
+            0.5f, -0.5f, 0.0f, // position
+            1.0f, 0.0f,        // uv
+            // bottom left
+            -0.5f, -0.5f, 0.0f, // position
+            0.0f, 0.0f,         // uv
+            // top left
+            -0.5f, 0.5f, 0.0f, // position
+            0.0f,
+            1.0f, // uv
+        };
 
-    Quad() {}
+        indices = {
+            0, 1, 3, // first tris
+            1, 2, 3  // second tris
+        };
+    }
 };
