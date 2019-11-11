b0c31b9c445bdf028c46b81c3718fd79a3928a50
nefarinus <tatonmarcinpr@gmail.com>
2019-11-11 13:31:04 +0100

    testing uv changing

--- src/meshes/Quad.h
+++ src/meshes/Quad.h
@@ -19,8 +19,7 @@ struct Quad : Mesh {
             0.0f, 0.0f,         // uv
             // top left
             -0.5f, 0.5f, 0.0f, // position
-            0.0f,
-            1.0f, // uv
+            0.0f, 1.0f,        // uv
         };
 
         indices = {
