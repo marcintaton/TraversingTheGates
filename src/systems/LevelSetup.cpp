2df5b5e65a4b50c8a085354792dc4731e6fbc6f8
nefarinus <tatonmarcinpr@gmail.com>
2019-11-11 20:06:08 +0100

    set camera on player on start

--- src/systems/LevelSetup.cpp
+++ src/systems/LevelSetup.cpp
@@ -1,5 +1,7 @@
 #include "LevelSetup.h"
 
+#include "../components/Transform.h"
+#include "../entities/Camera.h"
 #include "../systems/LevelGenerationManager.h"
 
 LevelSetup::LevelSetup()
@@ -27,4 +29,18 @@ void LevelSetup::setup_new_level(const OnLoadNewLevel* event)
 
     auto level_data =
         level_gen_man->dungeon_level_generator.get_generated_level<10, 10>();
+
+    auto player_id = ECS::ECEngine::get_instance()
+                         .get_entities_of_type<Player>()[0]
+                         ->get_entity_id();
+    auto player_trans =
+        ECS::ECEngine::get_instance().get_component<Transform>(player_id);
+
+    auto camera_id = ECS::ECEngine::get_instance()
+                         .get_entities_of_type<Camera>()[0]
+                         ->get_entity_id();
+    auto camera_trans =
+        ECS::ECEngine::get_instance().get_component<Transform>(camera_id);
+
+    camera_trans->position = player_trans->position;
 }
