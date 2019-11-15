39e84de5b9374d2382b642c0a5a0ad9a47028f9f
nefarinus <tatonmarcinpr@gmail.com>
2019-11-15 14:02:11 +0100

    typo :)

--- src/systems/LevelGenerationManager.h
+++ src/systems/LevelGenerationManager.h
@@ -3,8 +3,6 @@
 #include "../LevelGeneration/DungeonLevelGenerator.h"
 #include "../engine/GameEngine.h"
 
-class DungeonLevelGenerator;
-
 class LevelGenerationManager :
     public ECS::System::IndependentSystem<LevelGenerationManager>
 {
