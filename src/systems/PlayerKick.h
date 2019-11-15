2a89cdf7d46b2d150fe10f314f34689c599e9083
nefarinus <tatonmarcinpr@gmail.com>
2019-11-15 18:10:31 +0100

    started implementing kick system

---
+++ src/systems/PlayerKick.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+
+#include "../components/GameComponents.h"
+#include "../engine/GameEngine.h"
+#include "../entities/GameEntities.h"
+#include "../events/GameEvents.h"
+#include "../systems/GameSystems.h"
+
+class PlayerKick : public ECS::System::IndependentSystem<PlayerKick>
+{
+  private:
+};
